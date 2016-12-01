1ddd43d52ecf5099ac9259d07d55285ba9d31e45
James Mah <junyeumah@gmail.com>
2016-12-02 04:15:17 +1100

    removed console.js

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.use(bodyParser.json());
 app.use(methodOverride(function (req, res) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
     // look in urlencoded POST bodies and delete it
-    var method = req.body._method
-    delete req.body._method
-    return method
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
   }
-}))
+}));
 app.use(express.static(__dirname + '/public'));
 
 server.listen(port, function () {
