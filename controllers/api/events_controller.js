d00b486798878c39cf70c89b5853cc1390b0bcc3
Tim Walter <twwalter@gmail.com>
2016-12-02 17:23:44 +1100

    update controllers with new table data

--- controllers/api/events_controller.js
+++ controllers/api/events_controller.js
@@ -3,12 +3,12 @@ module.exports = function(app,pg,config){
   // GET '/api/events'
   app.get('/api/events', function(req, res) {
     // SQL query string
-    var query = "SELECT Events.id, Events.name, to_char(Events.date, 'DD Month YYYY') AS date, to_char(Events.time, 'HH12:MIAM') AS time, Events.description, Events.venue_id FROM Events";
+    var query = "SELECT Events.id, Events.name, to_char(Events.date, 'DD Month YYYY') AS date, to_char(Events.time, 'HH12:MIAM') AS time, Events.description, Events.venue_id, Events.image_url FROM Events";
 
     // Remove keys that are not valid
     keys = Object.keys(req.query);
     keys.filter(function(key) {
-      ['id', 'name', 'date', 'time', 'venue_id', 'presenter_id', 'user_id'].includes(key);
+      ['id', 'name', 'date', 'time', 'venue_id', 'presenter_id', 'user_id', 'image_url'].includes(key);
     });
 
     // If user is looking for events using presenter_id, join with Presenters_Users table
@@ -42,7 +42,7 @@ module.exports = function(app,pg,config){
   // GET '/api/events/:id'
   app.get('/api/events/:id', function(req, res) {
     // SQL query string
-    var  query = "SELECT id, name, to_char(date, 'DD Month YYYY') AS date, to_char(time, 'HH12:MIAM') AS time, description, venue_id FROM Events WHERE id=" + req.params.id;
+    var  query = "SELECT id, name, to_char(date, 'DD Month YYYY') AS date, to_char(time, 'HH12:MIAM') AS time, description, venue_id, image_url FROM Events WHERE id=" + req.params.id;
 
     // Retrieve data from Postgres and sent response to client
     var client = new pg.Client(config);
